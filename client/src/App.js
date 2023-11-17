ee005def3c8e3565ed4ca4775ede4127c199b46f
cyb3r-anarch <aanon408@gmail.com>
2023-11-16 20:34:56 -0800

    added upload and submit function

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,52 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async () => {
+    if (!selectedFile) {
+      alert('Please select a PDF file.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await fetch('https://<your-function-app-name>.azurewebsites.net/api/HttpExample', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log('Parsed text:', result.parsedText);
+        // Handle the parsed text as needed (display it, process it, etc.)
+      } else {
+        console.error('Error:', response.status, response.statusText);
+        // Handle error appropriately
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+      // Handle error appropriately
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Your azure website frontend is here!
-        </p>
+        <p>Your Azure website frontend is here!</p>
+        <div>
+          <input type="file" accept=".pdf" onChange={handleFileChange} />
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
